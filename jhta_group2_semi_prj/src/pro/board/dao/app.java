458c18e015624c7cbe55d3455bc1f1a1bee19241
JHTA <JHTA@JHTA-PC>
2017-09-05 14:07:45 +0900

    쓰레기 정리

--- jhta_group2_semi_prj/src/pro/board/dao/app.java
+++ jhta_group2_semi_prj/src/pro/board/dao/app.java
@@ -1,8 +1,24 @@
 package pro.board.dao;
 
+import java.sql.SQLException;
+import java.util.Date;
+
+import pro.postscription.vo.Postscription;
+
 public class app {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
-		AfterBoardDao.
+		AfterBoardDao adao = new AfterBoardDao();
+		Postscription pos = new Postscription();
+	    pos.setNo(1);
+	    pos.setGrade(1);
+	    pos.setTitle("d");
+	    pos.setContent("ds");
+	    pos.setRegdate("1234-11-22");
+	    pos.setStudent(student);
+	    
+		adao.AddAfterBoard(pos);
+		
+		
 	}
 }
