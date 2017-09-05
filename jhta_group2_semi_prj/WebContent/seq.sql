3b391f6e85a46231873cbf32344b781425a2819b
secr3t <kkandoli11@gmail.com>
2017-09-05 10:45:05 +0900

    SEQUENCE SQL

---
+++ jhta_group2_semi_prj/WebContent/seq.sql
@@ -0,0 +1,10 @@
+create sequence LECTURER_NO_SEQ START WITH 1 INCREMENT BY 1 maxvalue 999 nocache;
+create sequence DEPT_NO_SEQ START WITH 1 INCREMENT BY 1 maxvalue 99 NOCACHE;
+create sequence COURSE_NO_SEQ start with 1 increment by 1 maxvalue 999999 cache 100;
+create sequence ENROLLMENT_NO_SEQ start with 1 increment by 1 maxvalue 999999 cache 100;
+create sequence NOTICE_NO_SEQ start with 1 increment by 1 maxvalue 9999 nocache;
+create sequence TECH_NO_SEQ start with 1 increment by 1 maxvalue 9999 nocache;
+create sequence QNA_NO_SEQ start with 1 increment by 1 maxvalue 9999;
+create sequence STUDENT_NO_SEQ start with 1 increment by 1 maxvalue 999 nocache;
+create sequence COURSE_VIDEO_NO_SEQ start with 1 increment by 1 maxvalue 9999 nocache;
+create sequence POSTSCRIPTION_NO_SEQ start with 1 increment by 1 maxvalue 9999 nocache;
