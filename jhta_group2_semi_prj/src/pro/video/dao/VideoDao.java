92c6b9edd290a8156b79a5bf467dba8f8b90c5a5
secr3t <kkandoli11@gmail.com>
2017-09-05 16:00:14 +0900

    enrollment -> 주석처리 video mapper update

--- jhta_group2_semi_prj/src/pro/video/dao/VideoDao.java
+++ jhta_group2_semi_prj/src/pro/video/dao/VideoDao.java
@@ -1,14 +1,28 @@
 package pro.video.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import pro.utils.IbatisUtils;
 import pro.video.vo.Video;
 
 public class VideoDao {
 
+	private static VideoDao instance = new VideoDao();
+	public static VideoDao getInstance() {
+		return instance;
+	}
 	
 	public void addVideo(Video video) throws SQLException {
 		IbatisUtils.getSqlMap().insert("video.addVideo", video);
+	}
+	
+	public Video getVideoByNo(int no) throws SQLException{
+		return (Video) IbatisUtils.getSqlMap().queryForObject("video.getVideoByNo", no);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Video> getVideosByCourseNo(int no) throws SQLException{
+		return IbatisUtils.getSqlMap().queryForList("video.getVideosByCourseNo", no);
 	}
 }
