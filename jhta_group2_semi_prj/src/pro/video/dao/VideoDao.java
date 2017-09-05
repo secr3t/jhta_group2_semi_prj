5c8f216683fd1a56de5832c012450a1999786f61
secr3t <kkandoli11@gmail.com>
2017-09-05 12:16:32 +0900

    IbatisUtils 경로설정

--- jhta_group2_semi_prj/src/pro/video/dao/VideoDao.java
+++ jhta_group2_semi_prj/src/pro/video/dao/VideoDao.java
@@ -1,5 +1,14 @@
 package pro.video.dao;
 
+import java.sql.SQLException;
+
+import pro.utils.IbatisUtils;
+import pro.video.vo.Video;
+
 public class VideoDao {
 
+	
+	public void addVideo(Video video) throws SQLException {
+		IbatisUtils.getSqlMap().insert("video.addVideo", video);
+	}
 }
