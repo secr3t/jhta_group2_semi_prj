216f36afb306ec2f0edd0e617e91d6b90dad4db1
JHTA <JHTA@JHTA-PC>
2017-09-06 12:32:53 +0900

    criteria vo 생성

---
+++ jhta_group2_semi_prj/src/pro/criteria/vo/Criteria.java
@@ -0,0 +1,27 @@
+package pro.criteria.vo;
+
+public class Criteria {
+
+	private String opt;
+	private String keyword;
+	private int studentNo;
+	
+	public String getOpt() {
+		return opt;
+	}
+	public void setOpt(String opt) {
+		this.opt = opt;
+	}
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	public int getStudentNo() {
+		return studentNo;
+	}
+	public void setStudentNo(int studentNo) {
+		this.studentNo = studentNo;
+	}
+}
